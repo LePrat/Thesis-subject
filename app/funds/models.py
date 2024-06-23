63b27ecf5065bf02c13700fd301793da52c3519b
Kentin Pratelli <kentin.pratelli@epitech.eu>
2024-06-23 19:00:06 -0400

    add max_fund_amount & remove fund type

--- app/funds/models.py
+++ app/funds/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Fund(models.Model):
     name = models.CharField(max_length=200)
     funder = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, blank=True)
     nationality = models.CharField(max_length=200)
-    duration_in_months = models.PositiveIntegerField()
-    min_years_since_phd = models.PositiveIntegerField()
-    max_years_since_phd = models.PositiveIntegerField()
+    duration_in_months = models.PositiveIntegerField(blank=True)
+    min_years_since_phd = models.PositiveIntegerField(blank=True)
+    max_years_since_phd = models.PositiveIntegerField(blank=True)
+    max_fund_amount = models.CharField(max_length=200)
     link = models.URLField(max_length=200)
-    fund_type = models.CharField(max_length=10, choices=[("fellowship", "Fellowship"), ("grant", "Grant")])
 
     def __str__(self):
         return self.name
